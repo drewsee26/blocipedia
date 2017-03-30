b2398116f6aee4574d058b3e97dec0d030431bf6
Drew Hollenback <drewsee26@gmail.com>
2017-03-30 18:05:45 +0000

    users roles with default standard user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
   has_many :wikis
+  
+  after_initialize :user_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+         
+  enum role: [:standard, :premium, :admin]
+  
+  def user_role
+    self.role ||= :standard
+  end
 end

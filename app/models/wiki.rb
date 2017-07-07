4086f61e4dba4d9cd5c7591d29362ebdebd690d1
Drew Hollenback <drewsee26@gmail.com>
2017-07-07 22:46:52 +0000

    update action mailer for prod and basic user permissions

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,9 +2,15 @@ class Wiki < ActiveRecord::Base
   belongs_to :user
   has_many :collaborators
   has_many :users, through: :collaborators
+  before_save :set_private
   
   def collaborator_for(user)
     collaborators.find_by(user: user)
   end
   
+  private
+  
+  def set_private
+    self.private ||= false
+  end
 end
